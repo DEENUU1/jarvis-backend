f3b3d177441242c42abec1991344370256045872
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-16 23:54:09 +0100

    Create setup_memory function

---
+++ app/ai/memory.py
@@ -0,0 +1,22 @@
+from langchain.memory import ConversationBufferMemory
+from langchain_core.prompts import MessagesPlaceholder
+
+from .sql_chat_history import CustomSQLChatMessageHistory
+from config.settings import settings
+
+
+def setup_memory(session_id: str):
+    chat_message_history = CustomSQLChatMessageHistory(
+        session_id=session_id, connection_string=settings.SQLITE_CONNECTION_STRING,
+    )
+
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="history")],
+    }
+    memory = ConversationBufferMemory(
+        memory_key="history",
+        return_messages=True,
+        chat_memory=chat_message_history,
+    )
+
+    return agent_kwargs, memory
