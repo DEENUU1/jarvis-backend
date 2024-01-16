e16a495abcb3ea13e7050f5886009622679bc97e
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-16 23:49:59 +0100

    Create an endpoint to return messages for specified session

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -9,9 +9,21 @@ router = APIRouter(
 
 
 @router.get("", tags=["session_list"])
-def get_session_list(session_id: str = None):
+def get_session_list():
     unique_session = CustomSQLChatMessageHistory(
-        session_id=session_id,
+        session_id="None", # Session_id is not required here
         connection_string=settings.SQLITE_CONNECTION_STRING,
     ).unique_session_ids()
     print(unique_session)
+
+
+@router.get("/{session_id}", tags=["session_messages"])
+def get_session_messages(session_id: str):
+    messages = CustomSQLChatMessageHistory(
+        session_id=session_id,
+        connection_string=settings.SQLITE_CONNECTION_STRING,
+    ).get_messages_by_session_id(
+        target_session_id=session_id
+    )
+
+    print(messages)
