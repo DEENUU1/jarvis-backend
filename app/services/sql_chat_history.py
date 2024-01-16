6e70741bd8b28a77305a481bfb73301101f0a489
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-16 23:39:16 +0100

    Override SQLChatMessageHistory add method to return unique session ids and messages for specified session

---
+++ app/services/sql_chat_history.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+from langchain_core.messages import BaseMessage
+
+
+class CustomSQLChatMessageHistory(SQLChatMessageHistory):
+
+    def unique_session_ids(self) -> List[str]:
+        """
+        Retrieve all unique session IDs from db
+        """
+        with self.Session() as session:
+            result = (
+                session.query(getattr(self.sql_model_class, self.session_id_field_name))
+                .distinct()
+                .all()
+            )
+            return [row[0] for row in result]
+
+    def get_messages_by_session_id(self, target_session_id: str) -> List[BaseMessage]:
+        """
+        Retrieve messages for a specific session ID
+        """
+        with self.Session() as session:
+            result = (
+                session.query(self.sql_model_class)
+                .filter(
+                    getattr(self.sql_model_class, self.session_id_field_name)
+                    == target_session_id
+                )
+                .order_by(self.sql_model_class.id.asc())
+            )
+            messages = []
+            for record in result:
+                messages.append(self.converter.from_sql_model(record))
+            return messages
