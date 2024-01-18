37af0e68c107c32f26c04f0484c17a8897a48476
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-18 13:55:19 +0100

    Add middleware CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from config.settings import settings
 from routers import health, chat
 
@@ -13,3 +14,12 @@ app = FastAPI(
 
 app.include_router(health.router)
 app.include_router(chat.router)
+
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
