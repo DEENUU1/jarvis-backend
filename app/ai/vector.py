417fa9461893358fc6a7ec07505e265fa1b649ab
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-18 18:32:58 +0100

    Add endpoint to run embedding

--- app/ai/vector.py
+++ app/ai/vector.py
@@ -2,14 +2,10 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
 
-def save_to_chroma_db(data) -> bool:
-    try:
-        vector_db = Chroma.from_documents(
-            documents=data,
-            embedding=OpenAIEmbeddings(),
-            persist_directory="./data"
-        )
-        vector_db.persist()
-        return True
-    except:
-        return False
+def save_to_chroma_db(data):
+    vector_db = Chroma.from_documents(
+        documents=data,
+        embedding=OpenAIEmbeddings(),
+        persist_directory="./data"
+    )
+    vector_db.persist()
--- app/routers/media.py
+++ app/routers/media.py
@@ -1,8 +1,11 @@
+import os
 import shutil
 
 from fastapi import APIRouter
 from fastapi import File, UploadFile
 
+from ai.pdf import split_pdf
+from ai.vector import save_to_chroma_db
 
 router = APIRouter(
     prefix="/media",
@@ -21,3 +24,24 @@ def upload_file(uploaded_file: UploadFile = File(...)):
         "filename": uploaded_file.filename,
         "path": path
     }
+
+
+@router.post("/embedding")
+def run_embedding():
+    root_path = "media"
+
+    # Get all PDF files
+    pdf_files = [f for f in os.listdir(root_path) if f.endswith(".pdf")]
+
+    for file in pdf_files:
+        # Get path for file
+        file_path = os.path.join(root_path, file)
+        # Run function to split PDF into chunks and return it
+        chunks = split_pdf(file_path)
+        # Save chunks to chromadb
+        save_to_chroma_db(chunks)
+
+        # Delete file after processing
+        os.remove(file_path)
+
+
