519ca40d1bc90153ea04474d2105ee46c3339008
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-19 02:04:35 +0100

    Add DataLoaderFactory class

--- app/ai/vector.py
+++ app/ai/vector.py
@@ -1,6 +1,6 @@
 import os
 
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyPDFLoader, CSVLoader, JSONLoader, UnstructuredMarkdownLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Pinecone
@@ -30,13 +30,28 @@ def get_pinecone():
     )
 
 
+class DataLoaderFactory:
+    def split_docs(self, file_path: str):
+        if file_path.endswith(".pdf"):
+            return PyPDFLoader(file_path)
+        elif file_path.endswith(".csv"):
+            return CSVLoader(file_path)
+        elif file_path.endswith(".json"):
+            return JSONLoader(file_path)
+        elif file_path.endswith(".md"):
+            return UnstructuredMarkdownLoader(file_path)
+        else:
+            raise ValueError("Invalid file type")
+
+
 def split_files(file_path: str) -> ...:
     """
-    Split PDF into chunks
+    Split files into chunks
     """
-    loader = PyPDFLoader(file_path)
+    loader = DataLoaderFactory().split_docs(file_path=file_path)
     document = loader.load()
 
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     documents = text_splitter.split_documents(document)
     return documents
+
